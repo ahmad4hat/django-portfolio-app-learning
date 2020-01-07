987e592ae8245d7affc003b8b13110366ac1effd
ahmad4hat <rappar.stand@gmail.com>
2020-01-07 14:19:44 +0600

    static files added [resume]

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -125,7 +125,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'django_portfolio/static/')
+]
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = "/media/"
