7811f76fd30518034998fc55c695e010eee4559e
ahmad4hat <rappar.stand@gmail.com>
2020-01-07 13:21:04 +0600

    blog showing up on views

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+
+
+def allBlogs(request):
+    blogs = models.Blog.objects
+    return render(request, "blog/allblogs.html", {"blogs": blogs})
